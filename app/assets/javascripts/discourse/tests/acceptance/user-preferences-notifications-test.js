34de4eb7032892d33545d186cd5c09f1dd3b1121
Andrew Prigorshnev <a.prigorshnev@gmail.com>
2021-04-21 21:27:23 +0400

    More tests

--- app/assets/javascripts/discourse/tests/acceptance/user-preferences-notifications-test.js
+++ app/assets/javascripts/discourse/tests/acceptance/user-preferences-notifications-test.js
@@ -1,11 +1,13 @@
 import {
   acceptance,
   exists,
+  query,
   queryAll,
 } from "discourse/tests/helpers/qunit-helpers";
 import { click, visit } from "@ember/test-helpers";
 import selectKit from "discourse/tests/helpers/select-kit-helper";
 import { test } from "qunit";
+import I18n from "I18n";
 
 acceptance("User notification schedule", function (needs) {
   needs.user();
@@ -104,5 +106,44 @@ acceptance("User notification schedule", function (needs) {
       !selectKit(".day.Tuesday .ends-at .combobox").rowByValue(1350).exists(),
       "End time options are limited to + 30 past start time"
     );
+  });
+});
+
+acceptance("User notification - Users", function (needs) {
+  needs.user();
+
+  test("Ignore user - timeframe options", async function (assert) {
+    await visit("/u/eviltrout/preferences/users");
+    await click("div.user-notifications div div button");
+    await click(".future-date-input-selector-header");
+
+    assert.equal(
+      query(".future-date-input-selector-header").getAttribute("aria-expanded"),
+      "true",
+      "Selector is expanded"
+    );
+
+    const options = Array.from(
+      queryAll(`ul.select-kit-collection li span.name`).map((_, x) =>
+        x.innerText.trim()
+      )
+    );
+
+    const expected = [
+      I18n.t("topic.auto_update_input.tomorrow"),
+      I18n.t("topic.auto_update_input.later_this_week"),
+      I18n.t("topic.auto_update_input.this_weekend"),
+      I18n.t("topic.auto_update_input.next_week"),
+      I18n.t("topic.auto_update_input.two_weeks"),
+      I18n.t("topic.auto_update_input.next_month"),
+      I18n.t("topic.auto_update_input.two_months"),
+      I18n.t("topic.auto_update_input.three_months"),
+      I18n.t("topic.auto_update_input.four_months"),
+      I18n.t("topic.auto_update_input.six_months"),
+      I18n.t("topic.auto_update_input.one_year"),
+      I18n.t("topic.auto_update_input.forever"),
+    ];
+
+    assert.deepEqual(options, expected);
   });
 });
